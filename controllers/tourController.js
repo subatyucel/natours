a3610aee19e065bfe21b3c8304378cb2849c8e5f
subat <subat.yucel1@gmail.com>
2025-03-03 15:08:20 +0300

    advanced filtering added

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,11 +2,16 @@ const Tour = require('../models/tourModel');
 
 exports.getAllToursController = async (req, res) => {
   //Build query
+  //1- Filtering
   const queryObj = { ...req.query };
   const excludedFileds = ['page', 'sort', 'limit', 'fields'];
   excludedFileds.forEach((el) => delete queryObj[el]);
 
-  const query = Tour.find(queryObj);
+  //2- Advanced  Filtering
+  let queryStr = JSON.stringify(queryObj);
+  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+  const query = Tour.find(JSON.parse(queryStr));
   try {
     //Execute query
     const tours = await query;
