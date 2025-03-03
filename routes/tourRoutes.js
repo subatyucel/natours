ab820a357f7ac11364c3242634a87209e35974f6
subat <subat.yucel1@gmail.com>
2025-03-03 17:02:05 +0300

    top-5-cheap tours route added

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,10 @@ const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllToursController);
+
+router
   .route('/')
   .get(tourController.getAllToursController)
   .post(tourController.createTourController);
