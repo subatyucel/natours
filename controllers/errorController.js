0af103dd29552d838f3d82f0272b1d87948fd6a7
subat <subat.yucel1@gmail.com>
2025-03-05 20:39:11 +0300

    better error handling for production & development also checking if the error is created by us or unkown

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,37 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  //Operational messages that we created and trust
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    //Programming or other unknown error that we don't want to leak to client
+  } else {
+    //Log the error to the console
+    console.error('ERROR 👺', err);
+
+    //Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+
+  if (process.env.NODE_ENV === 'development') sendErrorDev(err, res);
+  if (process.env.NODE_ENV === 'production') sendErrorProd(err, res);
 };
