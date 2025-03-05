dd6d3b1f2dfa7b4fbca003e62a90d982c477ee5c
subat <subat.yucel1@gmail.com>
2025-03-05 21:11:10 +0300

    CastError Handled for user friendly message

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('../utils/AppError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value} `;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -33,5 +40,12 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') sendErrorDev(err, res);
-  if (process.env.NODE_ENV === 'production') sendErrorProd(err, res);
+  if (process.env.NODE_ENV === 'production') {
+    let error = Object.create(err);
+    if (error.name === 'CastError') {
+      error = handleCastErrorDB(error);
+    }
+
+    sendErrorProd(error, res);
+  }
 };
