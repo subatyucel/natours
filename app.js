3b64be04a1a465a8d2d17563c3fcc2832a786d81
subat <subat.yucel1@gmail.com>
2025-03-09 19:06:35 +0300

    hpp package implemented to prevent parameter polution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/AppError');
@@ -34,6 +35,20 @@ app.use(mongoSanitize());
 
 //Data santitazition againts XSS attacks
 app.use(xss());
+
+//Prevent parameter polition
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
 
 //Serving static files
 app.use(express.static(`${__dirname}/public`));
