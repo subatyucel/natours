045ef06b6b8b980ddeb393d5e5ce5f7e1eb6b9dd
subat <subat.yucel1@gmail.com>
2025-03-09 18:58:50 +0300

    express mongo santitizer and xss-clean packages implemented to obstruct NoSql injections and xss attacks

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/AppError');
@@ -26,6 +28,12 @@ app.use('/api', limiter);
 
 //Body parser - (MAX 10KB data on the body)
 app.use(express.json({ limit: '10kb' }));
+
+//Data santitazition againts NoSql query injection
+app.use(mongoSanitize());
+
+//Data santitazition againts XSS attacks
+app.use(xss());
 
 //Serving static files
 app.use(express.static(`${__dirname}/public`));
