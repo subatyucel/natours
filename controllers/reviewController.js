599023ed98265630f43eab3a20c507466eeac25d
subat <subat.yucel1@gmail.com>
2025-04-18 17:41:18 +0300

    tour review router changed to make it available for url params and logged in user

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,6 +2,9 @@ const Review = require('../models/reviewModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.createReviewController = catchAsync(async (req, res, next) => {
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+
   const newReview = await Review.create(req.body);
 
   res.status(201).json({
