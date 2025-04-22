608518b14b7d6775e7968df75ecb78d8d2a7f73a
subat <subat.yucel1@gmail.com>
2025-04-22 22:48:38 +0300

    user/me endpoint implemented to get current logged in user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -48,6 +48,10 @@ exports.deleteMeController = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
 exports.getAllUsersController = factory.getAll(User);
 exports.getUserByIdController = factory.getOne(User);
 exports.deleteUserController = factory.deleteOne(User);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,12 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+router.get(
+  '/me',
+  authController.protect,
+  userController.getMe,
+  userController.getUserByIdController,
+);
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/forgot-password', authController.forgotPassword);
