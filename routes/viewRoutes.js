a93a3e3d5b6e758128d26bb63756b6312bc2eb73
subat <subat.yucel1@gmail.com>
2025-08-04 17:16:17 +0300

    book functionality implemented insecure way it will changed with stripe web hooks  when app deployed on a server

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const viewController = require('../controllers/viewController');
 const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
-router.get('/', authController.isLoggedIn, viewController.getOverviewController);
+router.get(
+  '/',
+  bookingController.createBookingCheckoutController,
+  authController.isLoggedIn,
+  viewController.getOverviewController,
+);
 router.get('/tour/:slug', authController.isLoggedIn, viewController.getTourController);
 router.get('/login', authController.isLoggedIn, viewController.getLoginFormController);
 router.get('/me', authController.protect, viewController.getAccountController);
